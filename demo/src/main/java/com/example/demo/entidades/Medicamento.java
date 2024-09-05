a02437cb097cf78b4a835c33ebf79ff0b6272d2a
XDavidF29 <dp373401@gmail.com>
2024-09-04 23:25:33 -0500

    Sprint 5

---
+++ demo/src/main/java/com/example/demo/entidades/Medicamento.java
@@ -0,0 +1,114 @@
+package com.example.demo.entidades;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "MEDICAMENTOS_TABLE")
+public class Medicamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private float precio_compra;
+    private float precio_venta;
+    private int unidades_vendidas;
+    private int unidades_disponibles;
+
+    @ManyToMany(mappedBy = "medicamentos")  // Relación muchos a muchos bidireccional con Tratamiento
+    private List<Tratamiento> tratamientos = new ArrayList();
+    
+    //Constructor Vacio
+    public Medicamento() {
+    }
+
+    //Constructor
+    public Medicamento(Long id, String nombre, float precio_compra, float precio_venta, int unidades_vendidas,
+            int unidades_disponibles, List<Tratamiento> tratamientos) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio_compra = precio_compra;
+        this.precio_venta = precio_venta;
+        this.unidades_vendidas = unidades_vendidas;
+        this.unidades_disponibles = unidades_disponibles;
+        this.tratamientos = tratamientos;
+    }
+
+    //Constructor sin ID
+    public Medicamento(String nombre, float precio_compra, float precio_venta, int unidades_vendidas,
+            int unidades_disponibles, List<Tratamiento> tratamientos) {
+        this.nombre = nombre;
+        this.precio_compra = precio_compra;
+        this.precio_venta = precio_venta;
+        this.unidades_vendidas = unidades_vendidas;
+        this.unidades_disponibles = unidades_disponibles;
+        this.tratamientos = tratamientos;
+    }
+    //Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio_compra() {
+        return precio_compra;
+    }
+
+    public void setPrecio_compra(float precio_compra) {
+        this.precio_compra = precio_compra;
+    }
+
+    public float getPrecio_venta() {
+        return precio_venta;
+    }
+
+    public void setPrecio_venta(float precio_venta) {
+        this.precio_venta = precio_venta;
+    }
+
+    public int getUnidades_vendidas() {
+        return unidades_vendidas;
+    }
+
+    public void setUnidades_vendidas(int unidades_vendidas) {
+        this.unidades_vendidas = unidades_vendidas;
+    }
+
+    public int getUnidades_disponibles() {
+        return unidades_disponibles;
+    }
+
+    public void setUnidades_disponibles(int unidades_disponibles) {
+        this.unidades_disponibles = unidades_disponibles;
+    }
+
+    public List<Tratamiento> getTratamientos() {
+        return tratamientos;
+    }
+
+    public void setTratamientos(List<Tratamiento> tratamientos) {
+        this.tratamientos = tratamientos;
+    }
+        
+}
---
+++ demo/src/main/java/com/example/demo/entidades/Tratamiento.java
@@ -0,0 +1,121 @@
+package com.example.demo.entidades;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "TRATAMIENTO_TABLE")
+public class Tratamiento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Date fecha;
+    private int cantidad_mascotas;
+    private float precio;
+
+    @ManyToOne  // Relación con Mascota
+    private Mascota mascota;
+
+    @ManyToOne // Relación con veterinario
+    private Veterinario veterinario;
+
+    @ManyToMany //Relación con medicamentos
+    private List<Medicamento> medicamentos = new ArrayList();
+    
+    //Constructor Vacio
+    public Tratamiento() {
+    
+    }
+
+    //Constructor
+    public Tratamiento(Integer id, Date fecha, int cantidad_mascotas, float precio, Mascota mascota, Veterinario veterinario, List<Medicamento> medicamentos) {
+        this.id = id;
+        this.fecha = fecha;
+        this.cantidad_mascotas = cantidad_mascotas;
+        this.precio = precio;
+        this.mascota = mascota;
+        this.veterinario = veterinario;
+        this.medicamentos = medicamentos;
+    }
+
+    //Constructor sin ID
+    public Tratamiento(Date fecha, int cantidad_mascotas, float precio, Mascota mascota, Veterinario veterinario, List<Medicamento> medicamentos) {
+        this.fecha = fecha;
+        this.cantidad_mascotas = cantidad_mascotas;
+        this.precio = precio;
+        this.mascota = mascota;
+        this.veterinario = veterinario;
+        this.medicamentos = medicamentos;
+    }
+
+    
+    //Getter and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getCantidad_mascotas() {
+        return cantidad_mascotas;
+    }
+
+    public void setCantidad_mascotas(int cantidad_mascotas) {
+        this.cantidad_mascotas = cantidad_mascotas;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public Mascota getMascota() {
+        return mascota;
+    }
+
+    public void setMascota(Mascota mascota) {
+        this.mascota = mascota;
+    }
+
+    public Veterinario getVeterinario() {
+        return veterinario;
+    }
+
+    public void setVeterinario(Veterinario veterinario) {
+        this.veterinario = veterinario;
+    }
+
+    public List<Medicamento> getMedicamentos() {
+        return medicamentos;
+    }
+
+    public void setMedicamentos(List<Medicamento> medicamentos) {
+        this.medicamentos = medicamentos;
+    }
+
+}
