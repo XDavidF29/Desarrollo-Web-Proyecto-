1819fdfea00a55fe6eee13c719ce0f05acc9b57d
miguelbay <miguelblr26@gmail.com>
2024-09-12 17:00:50 -0500

    Modificaciones CRUD

--- demo/src/main/java/com/example/demo/entidades/Medicamento.java
+++ demo/src/main/java/com/example/demo/entidades/Medicamento.java
@@ -9,7 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-
+//Tratamientos
 @Entity
 @Table(name = "MEDICAMENTOS_TABLE")
 public class Medicamento {
